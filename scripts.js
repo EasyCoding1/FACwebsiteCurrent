a06a89a66655d12932e7c83e95a4b05466eedd74
easycoding1 <tsjr96@gmail.com>
2023-03-21 14:57:31 +0000

    Finished 2/4 modals

--- scripts.js
+++ scripts.js
@@ -1,25 +1,78 @@
-// Get the modal
-let modal = document.getElementById('myModal');
+// Get the modals by id
+let modal1 = document.getElementById("myModal1");
+let modal2 = document.getElementById("myModal2");
+let modal3 = document.getElementById("myModal3");
+let modal4 = document.getElementById("myModal4");
 
-// Get the button that opens the modal
-let btn = document.getElementById('mini3');
+// Get the buttons that open the modals
+let btn1 = document.getElementById("mini1");
+let btn2 = document.getElementById("mini2");
+let btn3 = document.getElementById("mini3");
+let btn4 = document.getElementById("mini4");
 
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName('close')[0];
+// Get the close buttons for each modal
+let close1 = document.getElementsByClassName("close")[0];
+let close2 = document.getElementsByClassName("close")[1];
+let close3 = document.getElementsByClassName("close")[2];
+let close4 = document.getElementsByClassName("close")[3];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// Open the modals when the buttons are clicked
+btn1.onclick = function() {
+  modal1.style.display = "block";
+};
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+btn2.onclick = function() {
+  modal2.style.display = "block";
+};
 
-// When the user clicks anywhere outside of the modal, close it
+btn3.onclick = function() {
+  modal3.style.display = "block";
+};
+
+btn4.onclick = function() {
+  modal4.style.display = "block";
+};
+
+// Close the modals when the close buttons are clicked
+close1.onclick = function() {
+  modal1.style.display = "none";
+};
+
+close2.onclick = function() {
+  modal2.style.display = "none";
+};
+
+close3.onclick = function() {
+  modal3.style.display = "none";
+};
+
+close4.onclick = function() {
+  modal4.style.display = "none";
+};
+
+// Close the modals when the user clicks anywhere outside the modal
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  } else if (event.target == modal2) {
+    modal2.style.display = "none";
+  } else if (event.target == modal3) {
+    modal3.style.display = "none";
+  } else if (event.target == modal4) {
+    modal4.style.display = "none";
   }
-}
+};
+
+
+
+
+
+// Find out more button scrolls to content NOT WORKING
+
+const findButton = document.getElementById(topbutton);
+const cntPlace = document.querySelector(content2);
+
+
+findButton.onclick = function() {
+  cntPlace.scrollIntoView
+};
